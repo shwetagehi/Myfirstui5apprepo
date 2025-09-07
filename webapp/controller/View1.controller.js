291bccf316491e90b78ea566b505f9be3f198328
Shweta Gehi <shwetagehi15@gmail.com>
2025-09-07 03:31:01 +0000

    setter getter demo

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -8,7 +8,21 @@ sap.ui.define([
         },
 
         onPress() {
+
+            //logic to navigate to second view
             this.getOwnerComponent().getRouter().navTo("RouteView2");
+        },
+
+        onSubmit(){
+
+            var name = this.getView().byId("idIpName").getValue(); //Get value of input field
+            var msg = "Welcome to "+name; //generate the dynamic text 
+            this.getView().byId("idTxtWelcome").setText(msg); //set the new text value to the text field
+            this.getView().byId("idBtnSubmit").setType("Accept"); //change button color to green
+            this.getView().byId("idTxtWelcome").setTextAlign("Left"); //set the changed text field alignment to left or right
+            this.getView().byId("idLblName").setRequired(false); //Make field as not mandatory
+            this.getView().byId("idIpName").setEnabled(false); //Disable input field
+
         }
     });
 });
