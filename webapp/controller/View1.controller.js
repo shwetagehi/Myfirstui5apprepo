5871266b3fef28a5af5691cdc5872d22c2d08af9
Shweta Gehi <shwetagehi15@gmail.com>
2025-09-13 00:00:12 +0000

    Mandatory field validations and data format validations

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -23,6 +23,51 @@ sap.ui.define([
             this.getView().byId("idLblName").setRequired(false); //Make field as not mandatory
             this.getView().byId("idIpName").setEnabled(false); //Disable input field
 
+        },
+
+        onValidate(){
+
+            var empid = this.getView().byId("idEmpid").getValue();
+            // Mandatory field validation
+            if (empid === "") {
+                this.getView().byId("idEmpid").setValueState("Error");
+                this.getView().byId("idEmpid").setValueStateText("Employee id is mandatory");
+            }
+            else{
+                this.getView().byId("idEmpid").setValueState("None");
+
+                //data format validations-
+                //Length validation
+                if (empid.length != 10) {
+                    this.getView().byId("idEmpid").setValueState("Error");
+                this.getView().byId("idEmpid").setValueStateText("Employee id should be 10 digits");
+                }
+
+                //alphabet, numeric & alpha-numeric validations using regular expressions
+
+                var regExp_alphabet = /^[a-zA-Z]+$/;
+                if (!empid.match(regExp_alphabet)) {
+                    this.getView().byId("idEmpid").setValueState("Error");
+                this.getView().byId("idEmpid").setValueStateText("Employee id must be alphabets");
+                }
+
+                 var regExp_numeric = /^[0-9]+$/;
+                 if (!empid.match(regExp_numeric)) {
+                    this.getView().byId("idEmpid").setValueState("Error");
+                this.getView().byId("idEmpid").setValueStateText("Employee id must be numbers");
+                }
+
+                  var regExp_aplhanumeric = /^[a-zA-Z0-9]+$/;
+                 if (!empid.match(regExp_alphanumeric)) {
+                    this.getView().byId("idEmpid").setValueState("Error");
+                this.getView().byId("idEmpid").setValueStateText("Employee id must be aphanumeric");
+                }
+
+            }
+
         }
+
+
+
     });
 });
